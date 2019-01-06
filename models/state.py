4644a6190264c874c0638b098e61df323eb34075
aslebloas <aslebloas@gmail.com>
2019-01-06 21:37:57 +0000

    Configure State class for DBStorage and FileStorage

--- models/state.py
+++ models/state.py
@@ -4,13 +4,23 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
         cities: reference to the City objects related to State
     """
+    self.name = ""
+    self.cities = ""
+
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
+    # For DBStorage
     cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    # For FileStorage
+    @property
+    def cities(self):
+        return self.cities
