84246d041034671772bdf26c71bab5b1ad49a8d2
aslebloas <aslebloas@gmail.com>
2019-01-07 19:11:49 +0000

    Solve query with DBStorage objects

--- models/state.py
+++ models/state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the state class"""
+import os
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -18,11 +19,12 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     # For DBStorage
-    cities = relationship("City",
-                          backref="state",
-                          cascade="all, delete, delete-orphan")
-
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     # For FileStorage
-    @property
-    def cities(self):
-        return self.cities
+    else:
+        @property
+        def cities(self):
+            return self.cities
