645302e8962d5b013e6a48de832079cf9d058801
Hunt6666 <489@holbertonschool.com>
2019-01-08 14:42:01 -0800

    cascade

--- models/review.py
+++ models/review.py
@@ -16,5 +16,5 @@ class Review(BaseModel, Base):
     """
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'))
-    user_id = Column(String(60), ForeignKey('users.id'))
+    place_id = Column(String(60), ForeignKey('places.id', ondelete="CASCADE"))
+    user_id = Column(String(60), ForeignKey('users.id', ondelete="CASCADE"))
