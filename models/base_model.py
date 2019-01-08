f1a0e53756ef5262667b81eeb7cb2be7d62f0945
aslebloas <aslebloas@gmail.com>
2019-01-08 19:18:04 +0000

    Modify the __str__ function - removed the to_dict() call

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
-    id = Column(String(60), primary_key=True)
+    id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
@@ -45,7 +45,7 @@ class BaseModel:
             returns a string of class name, id, and dictionary
         """
         return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.to_dict())
+            type(self).__name__, self.id, self.__dict__)
 
     def __repr__(self):
         """return a string representaion
