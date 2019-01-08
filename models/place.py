81b62f120e4c7819345a4684cc62877d8aa8b237
aslebloas <aslebloas@gmail.com>
2019-01-08 08:02:01 +0000

    Fixex dependencies

--- models/place.py
+++ models/place.py
@@ -23,7 +23,7 @@ class Place(BaseModel, Base):
     """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'))
+    city_id = Column(String(60), ForeignKey('cities.id', ondelete="CASCADE"))
     user_id = Column(String(60), ForeignKey('users.id', ondelete="CASCADE"))
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
--- models/state.py
+++ models/state.py
@@ -14,6 +14,8 @@ class State(BaseModel, Base):
     """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete, delete-orphan')
+    cities = relationship("City",
+                          backref="state",
+                          cascade='all, delete, delete-orphan')
 
     # Add getter for when storage is instance of FileStorage
