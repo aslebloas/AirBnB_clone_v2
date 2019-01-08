56256ec045ec92e61894e2b65d9d9ec37336f284
aslebloas <aslebloas@gmail.com>
2019-01-08 01:14:14 +0000

    Implement relationship with place and user TODO: cascade

--- models/place.py
+++ models/place.py
@@ -3,7 +3,8 @@
 from models.base_model import BaseModel, Base
 from models.city import City
 from models.user import User
-from sqlalchemy import Column, String, ForeignKey, Integer
+from sqlalchemy import Column, String, ForeignKey, Integer, Float
+
 
 class Place(BaseModel, Base):
     """This is the class for Place
@@ -22,14 +23,13 @@ class Place(BaseModel, Base):
     """
     __tablename__ = 'places'
 
-    city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'),
-                     ondelete='CASCADE')
+    city_id = Column(String(60), ForeignKey('cities.id'))
+    user_id = Column(String(60), ForeignKey('users.id'))
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
