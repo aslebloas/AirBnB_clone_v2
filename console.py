85c06f10a334b2c0dbc281973a5c5baa8af6a401
Hunt6666 <489@holbertonschool.com>
2019-01-08 13:19:07 -0800

    spaces work again

--- console.py
+++ console.py
@@ -43,11 +43,15 @@ class HBNBCommand(cmd.Cmd):
                 raise SyntaxError()
             my_list = line.split(" ")
             obj = eval("{}()".format(my_list[0]))
-            for i in range(len(my_list)):
+            i = 0
+            while i <= len(my_list):
                 try:
                     if i > 0:
                         param = my_list[i].split('=')
-                        if param[1][-1:] == '"':
+                        if param[1][0] == '"':
+                            while param[1][-1:] != '"':
+                                i += 1
+                                param[1] += ' ' + my_list[i]
                             param[1] = param[1].replace('_', ' ')
                             setattr(obj, param[0], str(param[1][1:-1]))
                         elif '.' in param[1]:
@@ -56,6 +60,7 @@ class HBNBCommand(cmd.Cmd):
                             setattr(obj, param[0], int(param[1]))
                 except:
                     pass
+                i += 1
             obj.save()
             print("{}".format(obj.id))
         except SyntaxError:
