9e4472947a77d707644cb21dc772e6d8acd6d4de
Hunt6666 <489@holbertonschool.com>
2019-01-09 12:26:31 -0800

    states

--- models/state.py
+++ models/state.py
@@ -28,7 +28,7 @@ class State(BaseModel, Base):
         """Getter for cities attr"""
         lst = []
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            return self.cities
+            return self.__cities
         else:
             for k, v in models.storage.all(City).items():
                 if v.state_id == self.id:
