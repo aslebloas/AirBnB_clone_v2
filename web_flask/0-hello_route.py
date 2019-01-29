d24cee03e6c693d049e736c8d9be11cbaa84d767
aslebloas <aslebloas@gmail.com>
2019-01-29 21:47:00 +0000

    Test

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Module"""
+"""Script that starts a Flask web application listening on 0.0.0.0:5000
+"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def index():
-    """Index method returns Hello HBNB!"""
-    return("Hello HBNB!")
+@app.route('/', strict_slashes=False)
+def say_hello():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
 
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
